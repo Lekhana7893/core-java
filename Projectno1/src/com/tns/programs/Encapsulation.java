12dcbbd85526baf8255b4e54532490908687f492
Bhargavi <Bhargavi@LAPTOP-N9Q0KJA7.Dlink>
2023-09-25 14:16:33 +0530

    first commit

---
+++ Projectno1/src/com/tns/programs/Encapsulation.java
@@ -0,0 +1,15 @@
+package com.tns.programs;
+// program to demonstrate encapsulation in java
+public class Encapsulation {
+	// datamembers
+    public int serialno;
+    public String name;
+    public int age;
+    
+    //membersfunction
+    
+    public void show() {
+     System.out.println("serialno"+ serialno+ "\t"+ "name"+ name+ "\t"+ "age"+ age);
+    }
+    
+}
