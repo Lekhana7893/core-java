aafbef68edb81d9452d99d8cfb8950642395d43e
Bhargavi <Bhargavi@LAPTOP-N9Q0KJA7.Dlink>
2023-10-03 15:29:27 +0530

    First Commit

---
+++ Projectno1/src/com/tns/inheritances/Bike.java
@@ -0,0 +1,18 @@
+package com.tns.inheritances;
+public class Bike {
+	private String companyName;
+	
+// set getters and setters
+	public String getCompanyName() {
+		return companyName;
+	}
+
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+// to string function
+	@Override
+	public String toString() {
+		return "Bike [companyName=" + companyName + "]";
+	}
+}
