8f2235500344c18cf3ec828476d42ad07e062ccd
Bhargavi <Bhargavi@LAPTOP-N9Q0KJA7.Dlink>
2023-10-03 19:12:12 +0530

    First Commit

---
+++ Projectno1/src/com/tns/overriding/Rbi.java
@@ -0,0 +1,9 @@
+package com.tns.overriding;
+//base class
+public class Rbi {
+	public float getRateofIntrest() {
+		return 6.7f;
+		
+	}
+
+}
---
+++ Projectno1/src/com/tns/overriding/Sbi.java
@@ -0,0 +1,9 @@
+package com.tns.overriding;
+
+public class Sbi extends Rbi {
+	public float getRateofIntrest() {
+			return 5.5f;
+			
+}
+		
+}
